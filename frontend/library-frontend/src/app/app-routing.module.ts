e4b04c941b44248020a427db2a0b2cd2804a46e7
StevenWeng60 <stevenweng60@gmail.com>
2023-08-07 12:46:51 -0500

    Added navbar and login page layout

--- frontend/library-frontend/src/app/app-routing.module.ts
+++ frontend/library-frontend/src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { BooksComponent } from './books/books.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'app', component: AppComponent},
+  { path: 'home', component: HomeComponent },
+  { path: 'books', component: BooksComponent },
+  { path: '**', component: LoginComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
